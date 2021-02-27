c67b65fab76fb67edf86376a101ad480f8b0f31e
rlftmdtp <rlfcksdlf@naver.com>
2021-02-27 11:19:23 +0900

    Programmers 문자열 다루기 기본

---
+++ src/Programmers/level1/StringHandle.java
@@ -0,0 +1,16 @@
+package Programmers.level1;
+
+import java.io.*;
+
+public class StringHandle { // 문자열 다루기 기본
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s = br.readLine();
+		
+		// 숫자로만 구성되어있는지 확인
+	}
+
+}

9eccddc10c7b8d8869d85196fb0e3a61a60da6e6
rlftmdtp <rlfcksdlf@naver.com>
2021-06-11 16:35:59 +0900

    Programmers level2 2020카카오 괄호변환

--- src/Programmers/level2/KakaoBlindRecruitment2020.java
+++ src/Programmers/level2/KakaoBlindRecruitment2020.java
@@ -1,7 +1,6 @@
 package Programmers.level2;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class KakaoBlindRecruitment2020 {
 
@@ -10,45 +9,54 @@ public class KakaoBlindRecruitment2020 {
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution("()))(( ()"));
+		System.out.println(solution("()))((()"));
 
 	}
 	
     public static String solution(String p) {
-        String answer = "";
-        
-        Queue<Character> u = new LinkedList<Character>();
-
-        
-        for(int i=0; i<p.length(); i++) {
-        	count(p.charAt(i));
-        	u.add(p.charAt(i));
-        	
-        	if(countEqual()) { // u과 균형잡힌 문자열일 경우
-        		if(u.peek() == '(') { // u가 올바른 괄호일 경우
-        			while(!u.isEmpty()) {
-        				answer += u.poll();
-        			}
-        			System.out.println("u가 올바른 괄호일 경우 " + answer);
-        		}else {	// u가 올바르지 않은 괄호일 경우
-        			answer += '(';
-        			int limit = u.size();
-        			int n = 0;
-        			while(!u.isEmpty()) {
-        				if(n == 0) u.poll(); // 첫글자 제거
-        				else if(n == limit-1) u.poll(); // 마지막글자 제거
-        				else { // 반대로 입력
-        					if(u.poll() == '(') answer += ')';
-        					else answer += '(';
-        				}
-        			}	
-        		}
-        	}
-        }
-        
-        return answer;
+        return recursion(0, p);
     }
    
+	private static String recursion(int i, String p) {
+		// TODO Auto-generated method stub
+		
+		if(i == p.length()) {
+			return "";
+		}else {
+			String u = "";
+			String v = "";
+			
+			// 두 문자열 u, v로 분리
+			int j;
+			for(j=i; j<p.length(); j++) {
+				count(p.charAt(j));
+				if(countEqual()) break;
+			}
+			
+			// j를 기준으로 u,v를 나눈다 
+			u = p.substring(i, j+1);
+			v = p.substring(j+1, p.length());
+			
+			// 올바른 괄호 문자열 
+			if(u.charAt(0) == '(') {
+				return u + recursion(j+1,p);
+			}else {
+				String newV = recursion(j+1,p);
+				StringBuilder sbV = new StringBuilder();
+				sbV.append("(");
+				sbV.append(newV);
+				sbV.append(")");
+				
+				StringBuilder sbU = new StringBuilder();
+				for(int z=1; z<u.length()-1; z++) { // u의 앞뒤 문자제거로 1시작과 끝 -1
+					if(u.charAt(z) == '(') sbU.append(")"); // 반대로 넣기
+					else sbU.append("(");
+				}
+				return sbV.toString() + sbU.toString();
+			}
+		}
+	}
+
 	public static void count(char c) {
     	if(c == '(') leftCount++;
     	else rightCount++;

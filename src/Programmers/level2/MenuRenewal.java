4a0a2d03c6e32254ae73264f347622aa899da3e7
rlftmdtp <rlfcksdlf@naver.com>
2021-06-15 17:16:26 +0900

    Programmers level2 2021카카오 메뉴리뉴얼

--- src/Programmers/level2/MenuRenewal.java
+++ src/Programmers/level2/MenuRenewal.java
@@ -3,52 +3,62 @@ package Programmers.level2;
 import java.util.*;
 
 public class MenuRenewal {
-	public static HashMap<String, Integer> hm = new HashMap<String, Integer>();
-	public static StringBuilder setMenu = new StringBuilder();
-	public static boolean visited;
+	public static HashMap<String, Integer> hm;
+	public static boolean visited[];
+	public static int MAX;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String orders[] = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
-		int course[] = {2,3,5};
+		String orders[] = {"XYZ", "XWY", "WXA"};
+		int course[] = {2,3,4};
 		System.out.println(solution(orders, course));
 	}
 	
     public static String[] solution(String[] orders, int[] course) {
-        String[] answer = {};
-        
-        for(String order : orders) {
-        	for(int setSize : course) {
-        		makeSetMenu(order, setSize, 0);
+    	
+        PriorityQueue<String> pq = new PriorityQueue<String>(); // 정답의 세트 메뉴명 오름차순 정렬을 위해 pq 사용
+        for(int i=0; i<course.length; i++) {
+        	int setSize = course[i];
+    		MAX = 0;
+    		hm = new HashMap<String, Integer>();
+        	for(String order : orders) {
+        		visited = new boolean[order.length()];
+        		char orderArray[] = order.toCharArray();
+        		Arrays.sort(orderArray); // 주문 메뉴 순서가 랜덤이므로 오름차순 정렬 시킴
+        		makeSetMenu(orderArray, setSize, 0);
+        	}
+        	
+        	for(String key : hm.keySet()) {
+        		if(hm.get(key) > 1 && hm.get(key) == MAX) pq.add(key);
         	}
         }
         
-        List<String> result = new ArrayList<String>();
-        for(String key : hm.keySet()) {
-        	if(hm.get(key) > 1) {
-        		result.add(key);
-        		//System.out.println("key " + key + " value " + hm.get(key) );
-        	}
+        String answer[] = new String[pq.size()];
+        int i = 0;
+        while(!pq.isEmpty()) {
+        	answer[i++] = pq.poll();
         }
-        
         return answer;
     }
 
-	private static void makeSetMenu(String order, int setSize, int count) {
+	private static void makeSetMenu(char orderArray[], int setSize, int depth) {
 		// TODO Auto-generated method stub
 		
-		// 순열과 조합의 차이 ..?
-		if(count == setSize) {
-			System.out.println("setMenu.toString() " + setMenu.toString());
-			hm.put(setMenu.toString(), hm.getOrDefault(setMenu.toString(), 0) +1);
-		}
-		else {
-			for(int i=count; i<order.length(); i++) {
-				setMenu.append(order.charAt(i));
-				makeSetMenu(order,setSize, count+1);
-				setMenu.deleteCharAt(setMenu.length()-1);
+		// 조합을 이용
+		if(setSize == 0) {
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<orderArray.length; i++) {
+				if(visited[i] == true) sb.append(orderArray[i]);
 			}
+			hm.put(sb.toString(), hm.getOrDefault(sb.toString(), 0) +1);
+			MAX = Math.max(MAX, hm.get(sb.toString()));
+		} else if(orderArray.length == depth) return;
+		else {
+			visited[depth] = true;
+			makeSetMenu(orderArray, setSize-1, depth+1); // 메뉴 하나를 선택 했을 때
+			visited[depth] = false;
+			makeSetMenu(orderArray, setSize, depth+1); // 메뉴 하나를 선택 했을 때
 		}
-	
 	}
 
 }

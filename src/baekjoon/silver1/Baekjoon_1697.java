b639b66b0ff286ee9484381e2041e0ae8c12dcd0
rlftmdtp <rlfcksdlf@naver.com>
2022-01-12 17:53:47 +0900

    백준 1697 숨바꼭질 아직 푸는 중

--- src/baekjoon/silver1/Baekjoon_1697.java
+++ src/baekjoon/silver1/Baekjoon_1697.java
@@ -6,7 +6,9 @@ import java.io.*;
 public class Baekjoon_1697 {
 	public static int N;
 	public static int K;
-	public static int TIME = Integer.MAX_VALUE;
+	public static int LIMIT = 0;
+	public static int visitied[];
+	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,49 +16,45 @@ public class Baekjoon_1697 {
 		String strSplit[] = br.readLine().split(" ");
 		N = Integer.parseInt(strSplit[0]);
 		K = Integer.parseInt(strSplit[1]);
+		LIMIT = Math.max(N, K);
+		visitied = new int[LIMIT+1];
 		// 수빈이가 동생을 찾을 수 있는 가장 빠른 시간
 		// 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동
-		bfs(N);
-		System.out.println(TIME);
+		System.out.println(bfs(N));
 	}
 	
-	public static void bfs(int start) {
-		Queue<Hidden> q = new LinkedList<>();
-		q.add(new Hidden(start, 0));
-		
-		while(!q.isEmpty()) {
-			Hidden h = q.poll();
-			
-			if(h.index == K) {
-				TIME = Math.min(h.time, TIME);
-			}
-			
-			for(int i=0; i<3; i++) {
-				if(i == 0) {
-					h.index -= 1;
-				}else if(i == 1) {
-					h.index += 1;
-				}else { // i == 2
-					h.index *= 2;
-				}
-				
+	public static int bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+		visitied[start] = 1;
+
+		while (!q.isEmpty()) {
+			int index = q.poll();
+
+			if (index == K)
+				return visitied[index]-1;
+
+			for (int i = 0; i < 3; i++) {
+				int next_index = 0;
+				if (i == 0)
+					next_index = index + 1;
+				else if (i == 1)
+					next_index = index - 1;
+				else
+					next_index = index * 2;
+
 				// 범위 확인
-				if(N<K) {
-					if(h.index >-1 && h.index<=K) q.add(new Hidden(h.index, h.time+1)); 
-				}else { // N>K
-					if(h.index>-1 && h.index<=N) q.add(new Hidden(h.index, h.time+1)); 
+				if (next_index >= 0 && next_index <= LIMIT){
+					if (visitied[next_index] == 0) {
+						System.out.println("next_index " + next_index);
+						visitied[next_index] = visitied[index] + 1;
+						q.add(next_index);
+					}
 				}
-			}	
+			}
 		}
+		return -1;
 	}
-
+	
 }
 
-class Hidden{
-	int index;
-	int time;
-	public Hidden(int index, int time) {
-		this.index = index;
-		this.time = time;
-	}
-}

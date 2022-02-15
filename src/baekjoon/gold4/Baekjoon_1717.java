14e6d153d0250219d93a80afd02c043887cd1dd3
rlftmdtp <rlfcksdlf@naver.com>
2022-02-15 18:09:27 +0900

    백준 집합의표현 푸는 중

---
+++ src/baekjoon/gold4/Baekjoon_1717.java
@@ -0,0 +1,24 @@
+package baekjoon.gold4;
+
+import java.io.*;
+import java.util.*;
+
+public class Baekjoon_1717 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String strSplit[] = br.readLine().split(" ");
+		
+		int N = Integer.parseInt(strSplit[0]);
+		int M = Integer.parseInt(strSplit[1]);
+		
+//		0 1 3
+//		1 1 7	
+		int list[] = new int[N+1];
+		for(int i=0; i<M; i++) {
+			
+		}
+	}
+
+}

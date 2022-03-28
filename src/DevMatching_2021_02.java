213b993ea09f9a498ab6e4da6b32cb150be86d68
rlftmdtp <rlfcksdlf@naver.com>
2022-03-28 18:28:11 +0900

    2022 웹 데브매칭 2번문제 어려워..

--- src/DevMatching_2021_02.java
+++ src/DevMatching_2021_02.java
@@ -2,83 +2,101 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
+import baekjoon.gold2.Baekjoon_11779.Node;
+
 
 public class DevMatching_2021_02 {
-	public static HashMap<String, Integer> hm = new HashMap<>();
 	
-	public static class Order implements Comparable<Order>{
-		int index;
-		int time;
-		
-		public Order(int index, int time){
-			this.index = index;
-			this.time = time;
-		}
-
-		@Override
-		public int compareTo(Order o) {
-			// TODO Auto-generated method stub
-			return o.index - this.index;
-		}
-		
-		
-	}
+	public static int MAP[][];
+	public static int direct_x[] = {0,1,1,1,0,-1,-1,-1};
+	public static int direct_y[] = {1,1,0,-1,-1,-1,0,1};
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		int n = 3;
+		// int n = 3;
 //		String[] recipies = {"A 3", "B 2"};
 //		String[] orders = {"A 1", "A 2", "B 3", "B 4"};
 		
-		String[] recipies = {"S 3", "F 2", "P 8"};
-		String[] orders = {"P 1", "F 2" , "S 4", "S 6", "P 7", "S 8"};
-		solution(n, recipies, orders);
+		int h = 7;
+		int w = 9;
+		int n  = 4;
+		String board[] = {"111100000", "000010011" , "111100011" , "111110011", "111100011" , "111100010" , "111100000"};
+		solution(h, w, n, board);
 	}
 	
-	public static int solution(int n, String[] recipes, String[] orders){
+	public static int solution(int h, int w, int n, String[] borad){
 		
-		for(int i=0; i<recipes.length; i++){
-			String strSplit[] = recipes[i].split(" ");
-			hm.put(strSplit[0], Integer.parseInt(strSplit[1]));
+		MAP = new int[h][w];
+		for(int i=0; i<h; i++) {
+			String str = borad[i];
+			for(int j=0; j<w; j++) {
+				MAP[i][j] = Integer.parseInt(str.charAt(j)+"");
+			}
 		}
 		
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		PriorityQueue<Order> result = new PriorityQueue<>();
-		int delayTime = 0;
-		for(int i=0; i<orders.length; i++){
-			String strSplit[] = orders[i].split(" ");
-			String recipie = strSplit[0];
+		int result = 0;
+		for(int i=0; i<h; i++) {
+			for(int j=0; j<w; j++) {
+				if(MAP[i][j] == 1) result += bfs(i,j,n);
+			}
+		}
+		System.out.println("최종 정답 " + result);
+		return result;
+	}
+	
+	public static int bfs(int x, int y, int limit) {
+
+		//총8개의 방향?
+		int count = 0;
+		for(int i=0; i<4; i++) {
+			Queue<Node> q = new LinkedList<>();
+			q.add(new Node(x,y));
+			int depth = 1;
 			
-			int orderTime = Integer.parseInt(strSplit[1]);
+			while(!q.isEmpty()) {
+				Node n = q.poll();
 			
-			if(pq.isEmpty()){
-				pq.add(delayTime + orderTime + cooking(recipie));
-			}else{
-				// 주문들어온 시간과 pq의 가장 빨리 끝나는 시간과 비교
-				while(pq.peek() <= orderTime && !pq.isEmpty()){
-					pq.poll();
+				int new_x = n.x + direct_x[i];
+				int new_y = n.y + direct_y[i];
+				// System.out.println("new_x " + new_x + " new_y " + new_y);
+				if(depth == limit) { // 마지막 돌  확인
+					int reverse_x = x + direct_x[i+4];
+					int reverse_y = y + direct_y[i+4];
+					if(new_x >= 0 && new_x<MAP.length && new_y >=0 && new_y < MAP[0].length &&
+							reverse_x >= 0 && reverse_x<MAP.length && reverse_y >=0 && reverse_y < MAP[0].length) { // 범위 확인
+						if(MAP[new_x][new_y] == 0 && MAP[reverse_x][reverse_y] == 0) { // 양쪽 끝으로 더 있는지 확인해야 한다
+							count++;
+							System.out.println("new_x " + new_x + " new_y " + new_y + " i방향 " + i);
+						}
+					}else {
+						count++;
+						System.out.println("new_x " + new_x + " new_y " + new_y + " i방향 " + i);
+					}
 				}
 				
-				// 만약에 빠진 시간이 없다
-				// delay 증가 후  제거를 하고 넣는다
-				if(pq.size() == n){
-					delayTime = pq.poll() - orderTime;
+				if(new_x >= 0 && new_x<MAP.length && new_y >=0 && new_y < MAP[0].length) { // 범위 확인
+					if(MAP[new_x][new_y] == 1) {
+						// System.out.println("zz");
+						q.add(new Node(new_x, new_y));
+						depth++;
+					}
 				}
-				System.out.println(i + " delayTime " + delayTime + " orderTime " + (delayTime + orderTime + cooking(recipie)));
-				pq.add(delayTime + orderTime + cooking(recipie));
 			}
-			result.add(new Order(i, delayTime + orderTime + cooking(recipie)));
 		}
+		System.out.println("X : " + x + " Y " + y + " count= " + count);
+		return count;
+	}
+	
+	public static class Node{
+		int x;
+		int y;
 		
-		int lastTime = result.poll().time;
-		System.out.println(lastTime);
-		return lastTime;
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
-	
-	public static int cooking(String recipie){
-		return hm.get(recipie);
-	}
-	
+
 	
 }
 

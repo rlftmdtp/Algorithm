32e2120e3e42fdb8e6d57072c55f9e5b0e871f4a
rlftmdtp <rlfcksdlf@naver.com>
2022-07-01 16:37:59 +0900

    leetCode SudokuSolver_37(완전 해결), ValidSudoku_36

--- src/leetcode/basic/SudokuSolver_37.java
+++ src/leetcode/basic/SudokuSolver_37.java
@@ -7,6 +7,7 @@ public class SudokuSolver_37 {
 		// Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
 		// Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
 		char[][] board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
+
 		solveSudoku(board);
 	}
 	
@@ -14,11 +15,18 @@ public class SudokuSolver_37 {
        
     	// BackTracking..
     	backTrack(board, 0, 0);
+    	
+    	for(int i=0; i<board.length; i++) {
+    		for(int j=0; j<board.length; j++) {
+    			System.out.print(board[i][j] + " ");
+    		}
+    		System.out.println();
+    	}
     }
     
     public static boolean backTrack(char[][] board, int x, int y) {
     	
-    	if(x == board.length && y == board[0].length) return true;
+    	if(x == board.length-1 && y == board[0].length) return true;
     	else {
 			// 다음 행 
 			if(y == board[0].length) {
@@ -29,14 +37,14 @@ public class SudokuSolver_37 {
 			if(board[x][y] == '.') {
 				// 숫자
 				for(int j=1; j<=9; j++) {
-					board[x][y] = (char)((int)'0' + j);
+					board[x][y] = (char)(j + (int)'0');
 					     
 					if(valid(board, x, y)) {
 						boolean tmp =  backTrack(board, x, y+1);
 						if(tmp == true) return true;
 					}
-				} d
-				
+				} 
+				board[x][y] = '.';
 			}else {
 				return backTrack(board, x, y+1);
 			}
@@ -46,31 +54,30 @@ public class SudokuSolver_37 {
     }
     
     public static boolean valid(char[][] board, int x, int y) {
-    	System.out.println("y = " + y );
     	// 가로 확인 
-    	boolean check[] = new boolean[9];
+    	boolean check[] = new boolean[10]; // 1~9 숫자 중복 확인
     	for(int i=0; i<9; i++) {
     		if(board[x][i] == '.') continue;
-    		if(check[board[x][i] - '0' -1] == true) return false;
-    		check[board[x][i] - '0' -1] = true;
+    		if(check[board[x][i] - '0'] == true) return false;
+    		check[board[x][i] - '0'] = true;
     	}
     	
     	// 세로 확인
-    	check = new boolean[9];
+    	check = new boolean[10];
     	for(int i=0; i<9; i++) {
     		if(board[i][y] == '.') continue;
-    		if(check[board[i][y] - '0' -1] == true) return false;
-    		check[board[i][y] - '0' -1] = true;
+    		if(check[board[i][y] - '0'] == true) return false;
+    		check[board[i][y] - '0'] = true;
     	}
     	
     	// 3 x 3 확인
-    	check = new boolean[9];
+    	check = new boolean[10];
     	for(int i=x/3 * 3 ; i<x/3 * 3 + 3; i++) {
     		for(int j=y/3 * 3; j<y/3 *3 + 3; j++) {
-    			System.out.println("i = " + i + "  j = " + j);
+    			// System.out.println("i = " + i + "  j = " + j);
         		if(board[i][j] == '.') continue;
-        		if(check[board[i][j] - '0' -1] == true) return false;
-        		check[board[i][j] - '0' -1] = true;
+        		if(check[board[i][j] - '0'] == true) return false;
+        		check[board[i][j] - '0'] = true;
     		}
     	}
     	
---
+++ src/leetcode/basic/ValidSudoku_36.java
@@ -0,0 +1,10 @@
+package leetcode.basic;
+
+public class ValidSudoku_36 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
